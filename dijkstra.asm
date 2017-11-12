070ad7b265369374795800aafcf1c0d41f4758a5
PratikBhusal <PratikBhusal@users.noreply.github.com>
2017-11-11 20:47:34 -0600

    Implement set_minimum_distance function in mips

--- dijkstra.asm
+++ dijkstra.asm
@@ -5,14 +5,15 @@
 
 	.data
 # {{{
-adj_matrix:	.word	0,3,5,9,0,0,3,0,3,4,7,0,5,3,0,2,6,0,9,4,2,0,2,2,0,7,6,2,0,5,0,0,0,2,5,0
-	# 0 3 5 9 0 0
-	# 3 0 3 4 7 0
-	# 5 3 0 2 6 0
-	# 9 4 2 0 2 2
-	# 0 7 6 2 0 5
-	# 0 0 0 2 5 0
+adj_matrix:	.word	0,3,5,9,0,0,
+			3,0,3,4,7,0,
+			5,3,0,2,6,0,
+			9,4,2,0,2,2,
+			0,7,6,2,0,5,
+			0,0,0,2,5,0
+
 distance:	.word	2147483647:7
+# distance:	.word	1,2,3,4,5,6,7
 is_shortest:	.word	0:7
 verticies:	.word	7
 
@@ -30,11 +31,12 @@ F:	.word	5
 main:
 # {{{
 	# Find Shortest Path from A to every other vertex {{{
-		# la	$a0,	adj_matrix
-		lw	$a1,	verticies
+		la	$a0,	adj_matrix
+		la	$a1,	verticies
 		lw	$a2,	A
 		jal	dijkstra
 	# Find Shortest Path from A to every other vertex }}}
+
 	# End Program {{{
 		j exit
 	# End Program }}}
@@ -46,13 +48,97 @@ dijkstra:
 	la	$s1,	is_shortest
 
 	# Set starting vertex to have a distance 0 {{{
-	sll	$t0,	$a2,	2	# $t0 is pointer to element we want
-	add	$t0,	$s0,	$t1	# Get pointer to element we want
-	lw	$t1,	0($t0)	# Load into $t1.
-	move	$t1,	$zero	# Modify $t1 to be 0
-	sw	$t1,	($s0)	# Store the value back into the distance array
+	addi	$sp,	$sp,	-4
+	sw	$ra,	($sp)
+	addi	$sp,	$sp,	-4
+	sw	$a0,	($sp)
+
+	la	$a0,	distance
+	jal	init_starting_vertex
+
+	lw	$a0,	($sp)
+	addi	$sp,	$sp,	4
+	lw	$ra,	($sp)
+	addi	$sp,	$sp,	4
 	# Set starting vertex to have a distance 0 }}}
 
+	li	$s6,	0	# int i = $s6 = 0
+	li	$s7,	0	# int j = $s7 = 0
+	dijkstra_main_loop:
+	#  {{{
+		# bge	$s6,	$s2,	dijkstra_main_loop # Check if we are are done with looping
+
+		# Set the current minimum distance {{{
+		addi	$sp,	$sp,	-4	# Put $ra into stack
+		sw	$ra,	($sp)
+		addi	$sp,	$sp,	-4	# Put distance arr into stack
+		sw	$s0,	($sp)
+		addi	$sp,	$sp,	-4	# Put is_shortest arr into stack
+		sw	$s1,	($sp)
+		addi	$sp,	$sp,	-4	# Put verticies into stack
+		sw	$a2,	($sp)
+
+		move	$a0,	$s0
+		move	$a1,	$s1
+		lw	$a2,	verticies
+		jal	set_minimum_distance
+
+		lw	$a2,	($sp)
+		addi	$sp,	$sp,	4
+		lw	$s1,	($sp)
+		addi	$sp,	$sp,	4
+		lw	$s0,	($sp)
+		addi	$sp,	$sp,	4
+		lw	$ra,	($sp)
+		addi	$sp,	$sp,	4
+		# Set the current minimum distance }}}
+	#  }}}
+
+	jr	$ra
+#  }}}
+
+init_starting_vertex: # Sets the starting vertex to have a distance 0
+#  {{{
+	sll	$t0,	$a2,	2	# $t0 is pointer to element we want
+	add	$t0,	$a0,	$t0	# Get pointer to element we want
+	lw	$t1,	0($t0)	# $t1 = distance[starting vertex]
+	move	$t1,	$zero	# distance[starting vertex] = 0
+	sw	$t1,	($t0)	# Store the value back into the distance array
+
+	jr	$ra
+#  }}}
+
+set_minimum_distance: # $a0 is distance $a1 is is_shortest $a2 is verticies
+#  {{{
+	li	$t0,	2147483647 # $t0 = min_value
+	li	$v0,	0	# $v0 = min_index
+
+	li	$t1,	0	#int i = $t1 = 0
+	set_minimum_distance_loop:
+	bge	$t1,	$a2,	end_set_minimum_distance
+
+	# if (*(found_path+i) == false && *(distance+i) <= min_value) {{{
+	sll	$t2,	$t1,	2	# Anchor pointer to element we want
+	add	$t2,	$a1,	$t2	# Set $t2 to address of is_shortest[i]
+	lw	$t3,	0($t2)	# $t3 = is_shortest[i]
+	seq	$t4,	$t3,	$zero	# Set $t6 to true if is_shortest[i] = false
+	beq	$t4,	$zero,	increment_set_minimum_distance_loop # Skip if $t4 is false
+
+	sll	$t2,	$t1,	2	# Anchor pointer to element we want
+	add	$t2,	$a0,	$t2	# Set $t2 to address of distance[i]
+	lw	$t3,	0($t2)	# $t3 = distance[i]
+	sle	$t4,	$t3,	$t0 # Set $t4 to 1 if distance[i] <= $t0
+	beq	$t4,	$zero,	increment_set_minimum_distance_loop # Skip if $t4 is false
+	#  }}}
+	move	$t0,	$t3 # min_value = *(distance + i);
+	move	$v0,	$t1 # min_index = i;
+
+	increment_set_minimum_distance_loop:
+
+	addi	$t1,	$t1,	1
+	j	set_minimum_distance_loop
+
+	end_set_minimum_distance:
 	jr	$ra
 #  }}}
 
